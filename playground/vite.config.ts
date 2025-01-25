ff1d75bcc898cefa2f3a33b149ba79ab4d8b60d5
livemehere <rhdxoals@gmail.com>
2025-01-26 03:09:35 +0900

    chore: add abs paths

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import resolvePaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [resolvePaths(), react()],
 });
