ada1593823f6aadeb13b1c94087f78bfa0d21f09
livemehere <rhdxoals@gmail.com>
2025-01-26 03:09:43 +0900

    chore: playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,35 +1,41 @@
 import "./App.css";
-import { useState } from "react";
-import { AnimateNumber } from "@fewings/fancy-react/AnimateNumber";
 import { css } from "@emotion/react";
-import { shade } from "@fewings/core/color";
-import { Space, Flex, Stack } from "@fewings/fancy-react/layout";
-import { addCommas } from "@fewings/core/math";
+import { Popover } from "@fewings/react/components";
+import { motion } from "motion/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <Space y={100} />
-      <Flex
-        align={"center"}
-        gap={10}
-        css={css`
-          background: #d2d2d2;
-          &:hover {
-            background: ${shade("#fff", -20)};
-          }
-        `}
-      >
-        <h1>hello</h1>
-        <div>bye</div>
-        <Stack gap={"33vh"}>
-          <h2>hello</h2>
-          <h2>hello</h2>
-        </Stack>
-      </Flex>
-      <AnimateNumber value={count} format={(v) => addCommas(v)} />
+      <Popover.Root type={"hover"}>
+        <Popover.Trigger>
+          <h1
+            css={css`
+              cursor: pointer;
+              margin: 0;
+              padding: 24px;
+              background: #f0f0f0;
+            `}
+          >
+            hello
+          </h1>
+        </Popover.Trigger>
+        <Popover.Panel anchor={"right-center"}>
+          <motion.div
+            animate={{
+              x: [-10, 0],
+              opacity: [0, 1],
+            }}
+            css={css`
+              width: 200px;
+              height: 200px;
+              background: #424242;
+              //margin-top: 15px;
+            `}
+          >
+            world
+          </motion.div>
+        </Popover.Panel>
+      </Popover.Root>
     </div>
   );
 }
