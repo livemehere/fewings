60ae5311c202214ef389995ff9b402393c91ed7a
livemehere <rhdxoals@gmail.com>
2025-02-02 03:58:20 +0900

    chore: playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -3,17 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Page2 from "./pages/Page2";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
+import { AnimatePresence } from "motion/react";
+import { OverlayProvider } from "@fewings/react/overlay";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/page2"} element={<Page2 />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <OverlayProvider wrapper={AnimatePresence}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/page2"} element={<Page2 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </OverlayProvider>
   );
 }
 
--- playground/src/pages/Home.tsx
+++ playground/src/pages/Home.tsx
@@ -1,10 +1,7 @@
-import { Icon } from "../Icon/Icon";
-
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <Icon name={"icons/vite2"} width={500} height={500} />
+      <h1>Playground</h1>
     </div>
   );
 };
