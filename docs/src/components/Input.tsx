741565c25c63ca0ed35b001496a6a2a268f26a74
livemehere <rhdxoals@gmail.com>
2025-02-03 00:38:29 +0900

    docs: fancy-react

---
+++ docs/src/components/Input.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ComponentProps } from "react";
+
+const Input = (props: ComponentProps<"input">) => {
+  return (
+    <input
+      {...props}
+      style={{
+        padding: 8,
+        background: "rgba(255,255,255, 0.05)",
+        border: "1px solid rgba(255,255,255, 0.1)",
+        borderRadius: 4,
+        ...props.style,
+      }}
+    />
+  );
+};
+
+export default Input;
---
+++ docs/src/content/fancy-react/AnimateNumberExample.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { rand } from "@fewings/core/math";
+import { AnimateNumber } from "@fewings/fancy-react";
+import { withCommas } from "@fewings/core/converter";
+import { HStack, Space, VStack } from "@fewings/react/components";
+import Input from "@/components/Input";
+
+const nums = [0, 12030, 12445823, 938293, 2353, 2999993435, 24];
+
+const getRandNum = () => nums[rand(0, nums.length - 1, true)];
+
+const AnimateNumberExample = () => {
+  const [value, setValue] = useState(nums[0]);
+  const [countDur, setCountDur] = useState(0.8);
+  const [sizeDur, setSizeDur] = useState(0.3);
+
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      setValue(getRandNum());
+    }, 1500);
+    return () => {
+      window.clearInterval(id);
+    };
+  }, []);
+
+  return (
+    <div>
+      <Space y={40} />
+      <HStack justify={"center"} gap={20}>
+        <VStack align={"center"} gap={8}>
+          <Input
+            type="number"
+            step={0.01}
+            value={countDur}
+            onChange={(e) => setCountDur(Number(e.target.value))}
+          />
+          <div>Rolling Duration</div>
+        </VStack>
+        <VStack align={"center"} gap={8}>
+          <Input
+            type="number"
+            step={0.01}
+            value={sizeDur}
+            onChange={(e) => setSizeDur(Number(e.target.value))}
+          />
+          <div>Sizing Duration</div>
+        </VStack>
+      </HStack>
+      <Space y={50} />
+      <HStack justify={"center"}>
+        <AnimateNumber
+          value={value}
+          countDur={countDur}
+          sizeDur={sizeDur}
+          format={(v) => withCommas(v)}
+          style={{
+            fontSize: 38,
+            fontWeight: "bold",
+          }}
+        />
+      </HStack>
+    </div>
+  );
+};
+
+export default AnimateNumberExample;
