ff84ea761c31f27ee24c46766c79f56cb871df7c
livemehere <rhdxoals@gmail.com>
2025-02-02 19:57:49 +0900

    docs: shade

---
+++ docs/src/content/core/color/ShadeExample.tsx
@@ -0,0 +1,57 @@
+import { shade } from "@fewings/core/color";
+
+const originColor = "#de4343";
+const colors = [
+  {
+    color: shade(originColor, 0),
+    value: 0,
+  },
+  {
+    color: shade(originColor, 30),
+    value: 30,
+  },
+  {
+    color: shade(originColor, -30),
+    value: -30,
+  },
+];
+
+const ShadeExample = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: 10,
+        justifyContent: "center",
+        padding: 40,
+      }}
+    >
+      {colors.map(({ color, value }) => (
+        <div
+          style={{
+            backgroundColor: color,
+            color: "white",
+            width: 200,
+            height: 80,
+            fontWeight: "bold",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div>{color}</div>
+          <div
+            style={{
+              fontSize: 14,
+            }}
+          >
+            shade({originColor},{value})
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ShadeExample;
