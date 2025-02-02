6d92a388d84dac459356be6f686d547a70669822
livemehere <rhdxoals@gmail.com>
2025-02-03 00:39:41 +0900

    publish:
    - fancy-react : 0.0.4
    - react : 0.0.9

--- playground/src/pages/Home.tsx
+++ playground/src/pages/Home.tsx
@@ -1,7 +1,28 @@
+import { HStack, VStack } from "@fewings/react/components";
+
 const Home = () => {
   return (
     <div>
       <h1>Playground</h1>
+      <VStack gap={4}>
+        <HStack
+          justify={"center"}
+          gap={20}
+          style={{
+            padding: 20,
+            background: "#eee",
+            color: "black",
+          }}
+        >
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </HStack>
+        <HStack>htllo</HStack>
+        <HStack>htllo</HStack>
+        <HStack>htllo</HStack>
+        <HStack>htllo</HStack>
+      </VStack>
     </div>
   );
 };
