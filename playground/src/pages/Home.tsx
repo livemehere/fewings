24b4e82e30a5e50720411515658212bbaf012caf
livemehere <rhdxoals@gmail.com>
2025-03-02 17:41:54 +0900

    chore: playground
    add jotai

--- playground/src/pages/Home.tsx
+++ playground/src/pages/Home.tsx
@@ -1,61 +1,23 @@
-import { AnimatePresence, motion } from "motion/react";
-// import { Popover, Accordion } from "../../../packages/react/src/components";
 import { css } from "@emotion/react";
-import { useState } from "react";
-import { Accordion } from "@fewings/react/components";
+import { useControlledState } from "../../../packages/react/src/hooks/useControlledState";
+import { atom, useAtom } from "jotai";
+
+const vAtom = atom("");
 
 const Home = () => {
-  const [open, setOpen] = useState(false);
-  const [disabled, setDisabled] = useState(false);
+  const [g, setG] = useAtom(vAtom);
+  const [v, setV] = useControlledState({
+    defaultValue: "hello",
+    value: g,
+    onChange: (v) => {
+      setG(v);
+    },
+  });
+
   return (
-    <div
-      css={css`
-        height: 200vh;
-      `}
-    >
-      <h1>Playground</h1>
-      <Accordion.Root disabled={disabled} initialOpen={true}>
-        <Accordion.Trigger>
-          <button>Trigger</button>
-        </Accordion.Trigger>
-        <Accordion.Panel wrapper={AnimatePresence}>
-          <motion.ul
-            animate={{
-              scale: [0, 1],
-            }}
-            exit={{
-              scale: [2, 0],
-            }}
-            css={css`
-              margin: 0;
-              background: #6a737d;
-            `}
-          >
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </motion.ul>
-        </Accordion.Panel>
-      </Accordion.Root>
-      <div>
-        <p>open : {open ? "true" : "false"}</p>
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            setOpen(!open);
-          }}
-        >
-          TOGGLE
-        </button>
-        <button
-          onClick={() => {
-            setDisabled(!disabled);
-          }}
-        >
-          DISABLE
-        </button>
-      </div>
+    <div css={css``}>
+      <h1>hello world</h1>
+      <input type="text" value={v} onChange={(e) => setV(e.target.value)} />
     </div>
   );
 };
