78f012186ebad4663682c2e4b927d48e87399930
livemehere <rhdxoals@gmail.com>
2025-04-20 05:49:00 +0900

    refactor: core/hash folder structure

--- packages/core/src/hash/index.ts
+++ packages/core/src/hash/index.ts
@@ -1,7 +1,1 @@
-export function djb2(str: string) {
-  let hash = 5381;
-  for (let i = 0; i < str.length; i++) {
-    hash = (hash << 5) + hash + str.charCodeAt(i);
-  }
-  return hash;
-}
+export * from "./djb2";
