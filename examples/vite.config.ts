1ec97811564e043992db985404e3349cf621fee2
livemehere <rhdxoals@gmail.com>
2025-04-20 23:27:39 +0900

    fix: add tsconfig paths plugin to Vite configuration

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,3 +1,5 @@
 import { defineConfig } from "vite";
-
-export default defineConfig({});
+import tsconfigPaths from "vite-tsconfig-paths";
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+});
